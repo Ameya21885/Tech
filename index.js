01cfcd27593c464de4b6f4fd63a3d04f2a0c0e88
Ameya21885 <82252968+Ameya21885@users.noreply.github.com>
2021-12-28 12:34:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+function myFunction() {
+    document.body.style.backgroundColor=" rgb(53, 47, 47)";
+    document.body.style.color="white";
+ }
+
+ function myFunction1() {
+    document.body.style.backgroundColor="white";
+    document.body.style.color="black";
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
